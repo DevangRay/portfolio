04371ab4849d4a3b98d3c72a3815be4e5efba12f
Devang Ray <devangray624@gmail.com>
2024-12-04 23:41:07 -0500

    Changed name of Resume but did not update src for iframe

--- src/components/PDFFrame.tsx
+++ src/components/PDFFrame.tsx
@@ -19,7 +19,7 @@ export default function PDFFrame() {
                 Center PDF
             </button> */}
             <div className="h-screen" ref={frameRef}>
-                <iframe src="Devang Ray Resume.pdf" width="100%" height="100%"/>
+                <iframe src="Resume.pdf" width="100%" height="100%"/>
             </div>
         </>
     );
