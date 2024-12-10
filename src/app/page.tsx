5d808f5b6b7657a1b01f452ca06d1000c40152c2
Devang Ray <devangray624@gmail.com>
2024-12-10 16:08:03 -0500

    changing env variable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { Highlight } from "@/components/Highlight";
 import { Paragraph } from "@/components/Paragraph";
 import { Products } from "@/components/Products";
 import { TechStack } from "@/components/TechStack";
+import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
     <Container>
       <span className="text-4xl">👋</span>
       <Heading className="font-black">Hello there! I&apos;m Devang</Heading>
+      <Heading>{process.env.NEXT_POSTHOG_KEY}</Heading>
       <Heading
         as="h2"
         className="font-black text-lg md:text-lg lg:text-lg mt-20 mb-4"
